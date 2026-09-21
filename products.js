fdf65916dbc47b67e445dcca92c124863f6157d2
Mastain2003 <prakharmastain9@gmail.com>
2026-09-21 18:27:04 +0530

    Update products.js

--- products.js
+++ products.js
@@ -3,6 +3,26 @@
 const PRODUCTS = [
   {
     id: "product-001",
+    title: "Sanchi Milk Cake 250gm",
+    brand: "Sanchi",
+    description: "Sanchi Milk Cake 250gm made up of pure mawa..",
+    price: 125,
+    salePrice: 115,
+    currency: "INR",
+    availability: "In Stock",
+    condition: "New",
+    sku: "SKU-001",
+    category: "Sweets",
+    images: [
+      "img/milk cake 250/file_0000000090f081fa8e8cea8a38b924d7(3).png"
+    ],    
+    specs: {
+      "Validity": "1 Month",
+      "Country of origin": "India"
+    }
+  },
+  {
+    id: "product-002",
     title: "Sanchi Peda 250gm",
     brand: "Sanchi",
     description: "Sanchi peda 250gm made up of pure mawa..",
@@ -11,41 +31,16 @@ const PRODUCTS = [
     currency: "INR",
     availability: "In Stock",
     condition: "New",
-    sku: "SKU-001",
-    category: "Sweets",
-    images: [
-      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?auto=format&fit=crop&w=1200&q=80",
-      "https://images.unsplash.com/photo-1524805444758-089113d48a6d?auto=format&fit=crop&w=1200&q=80"
-    ],
-    variants: [
-      { name: "Color", options: ["Mulberry", "Black", "White"] },
-      { name: "Size", options: ["Small", "Medium", "Large"] }
-    ],
-    specs: {
-      "Validity": "1 Month",
-      "Country of origin": "India"
-    }
-  },
-  {
-    id: "product-002",
-    title: "Sanchi Peda 500gm",
-    brand: "Sanchi",
-    description: "Sanchi peda 500gm made up of pure mawa..",
-    price: 230,
-    salePrice: 210,
-    currency: "INR",
-    availability: "In Stock",
-    condition: "New",
     sku: "SKU-002",
     category: "Sweets",
     images: [
-      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?auto=format&fit=crop&w=1200&q=80",
-      "https://images.unsplash.com/photo-1524805444758-089113d48a6d?auto=format&fit=crop&w=1200&q=80"
-    ],
-    variants: [
-      { name: "Color", options: ["Mulberry", "Black", "White"] },
-      { name: "Size", options: ["Small", "Medium", "Large"] }
-    ],
+      "img/peda 250/1789992001302.png(1)",
+      "img/peda 250/1789992001302.png(2)",
+      "img/peda 250/1789992001302.png(3)",
+      "img/peda 250/1789992001302.png(4)",
+      "img/peda 250/1789992001302.png(5)",
+      "img/peda 250/1789992001302.png"
+    ],    
     specs: {
       "Validity": "1 Month",
       "Country of origin": "India"
